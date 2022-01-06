6a3b4d85e7dfaf7cb52b67c024a73fa17c71acfa
Judith V. C <judithvc2209@gmail.com>
2022-01-06 13:36:19 +0100

    encuestas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,14 @@ import { Component } from '@angular/core';
 export class AppComponent {
   mostrarForm = false;
 
-  encuesta = {
+  encuestas = [{
     pregunta: '¿Cuál es tu macota favorita?',
-    resultados: [0, 4, 3, 2, 1],
+    resultados: [0, 4, 3, 2],
     votada: true
-  };
+  },
+  {
+    pregunta: '¿Cuál es tu día de la semana favorito?',
+    resultados: [2, 7, 9, 3],
+    votada: false
+  }]
 }

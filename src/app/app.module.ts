e85046ca906a11335bbd9b17caac278e66dd7a61
Judith V. C <judithvc2209@gmail.com>
2022-01-06 11:54:50 +0100

    publicar encuesta logica en proceso

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CrearEncuestaComponent } from './crear-encuesta/crear-encuesta.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CrearEncuestaComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/crear-encuesta/crear-encuesta.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-crear-encuesta',
+  templateUrl: './crear-encuesta.component.html',
+  styleUrls: ['./crear-encuesta.component.css']
+})
+export class CrearEncuestaComponent {
+
+  constructor() { }
+
+}
